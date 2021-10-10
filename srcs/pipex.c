ff8fffa9ae980787e9f605ebf4041d5efe2cf1be
Neyune <ereali@e3r10p8.42.fr>
2021-10-10 02:17:32 +0200

    fork

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ereali <ereali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 19:19:09 by ereali            #+#    #+#             */
-/*   Updated: 2021/10/08 18:19:03 by ereali           ###   ########.fr       */
+/*   Updated: 2021/10/10 02:16:48 by ereali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,37 +25,51 @@ void args_error(void)
 // 	return (1);
 // }
 
-int	main(int argc, char** argv)
+int	main(int argc, char **argv, char **envp)
 {
 	int pipefd[2];
 	int in_pid;
+	int first_pid;
+	int second_pid;
+	int *status;
 	int i;
 
 	i = 1;
+	(void)envp;
 	if (argc != 5)
-		args_error();
-	in_pid = open(argv[1], O_RDONLY);
-	if (in_pid == -1)
 		args_error();
 	if (pipe(pipefd) == -1)
 	{
 		perror("pipe");
 		exit(EXIT_FAILURE);
     }
-	in_pid = fork();
-    if (in_pid == -1)
+	child = fork();
+    if (child < 0)
 	{
 		perror("fork");
 		exit(EXIT_FAILURE);
     }
-	if (in_pid == 0)
+	if (child == 0)
 	{
-		close(pipefd[0]);
-
+		first_pid = open(argv[1], O_RDONLY);
+		if (first_pid < 0)
+			args_error();
 	}
-	else
+	child2 = fork();
+	if (child2 < 0)
 	{
-
+		perror("fork");
+		exit(EXIT_FAILURE);
 	}
+	if (child2 == 0)
+	{
+		second_pid = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+		if (second_pid < 0)
+			args_error();
+	}
+	waitpid(child, &status, 0);
+	close(pipefd[0]);
+	waitpid(child2, &status, 0);
+	close(pipefd[1]);
 	return (0);
 }
