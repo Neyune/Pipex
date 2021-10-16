c19df24049a6dfd8c2eea06e35e10f3d77f87366
Neyune <ereali@e2-standing-desk.42.fr>
2021-10-16 23:15:12 +0200

    pipe

--- srcs/Basic.c
+++ srcs/Basic.c
@@ -6,7 +6,7 @@
 /*   By: ereali <ereali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 19:37:08 by ereali            #+#    #+#             */
-/*   Updated: 2021/09/26 19:40:18 by ereali           ###   ########.fr       */
+/*   Updated: 2021/10/15 21:49:55 by ereali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,98 @@ void	ft_putstr_fd(char *s, int fd)
 		write(fd, &s[i], 1);
 		i++;
 	}
+}
+
+static size_t	ft_count_word(char const *s, char c)
+{
+	size_t	i;
+	size_t	word;
+
+	i = 0;
+	word = 0;
+	if (ft_strlen(s) == 0)
+		return (0);
+	while (s[i] == c)
+		i++;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			word++;
+			while (s[i] == c)
+				i++;
+			if (!(s[i]))
+				i--;
+		}
+		i++;
+	}
+	if (s[i - 1] == c)
+		return (word);
+	return (word + 1);
+}
+
+static char	*ft_get_next_word(char const *s, char c, size_t *j)
+{
+	char	*word;
+	size_t	jcopy;
+	size_t	i;
+
+	i = 0;
+	while (s[*j] == c)
+		(*j)++;
+	jcopy = *j;
+	while (s[jcopy] && s[jcopy] != c)
+		jcopy++;
+	word = (char *)malloc(sizeof(char) * (jcopy - *j + 1));
+	if (!(word))
+		return (NULL);
+	while (*j < jcopy)
+	{
+		word[i] = s[*j];
+		i++;
+		(*j)++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+static void	ft_freetablib(char **tab, int i)
+{
+	while (i >= 0)
+	{
+		free(tab[i]);
+		i--;
+	}
+	free(tab);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**tab;
+	size_t	i;
+	size_t	j;
+	size_t	word;
+
+	i = 0;
+	j = 0;
+	if (!(s))
+		return (NULL);
+	word = ft_count_word(s, c);
+	if (word <= 1)
+		return(NULL);
+	tab = (char **)malloc(sizeof(char *) * (word + 1));
+	if (!(tab))
+		return (NULL);
+	while (i < word)
+	{
+		tab[i] = ft_get_next_word(s, c, &j);
+		if (!(tab[i]))
+		{
+			ft_freetablib(tab, i);
+			return (NULL);
+		}
+		i++;
+	}
+	tab[word] = NULL;
+	return (tab);
 }
--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,70 +6,86 @@
 /*   By: ereali <ereali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 19:19:09 by ereali            #+#    #+#             */
-/*   Updated: 2021/10/10 02:16:48 by ereali           ###   ########.fr       */
+/*   Updated: 2021/10/15 21:53:42 by ereali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void args_error(void)
+void	args_error(void)
 {
 	ft_putstr_fd("Wrong args", 2);
 	exit(EXIT_FAILURE);
 }
 
-// int	check_arg(char *s)
-// {
-// 	if (!s)
-// 		return (0);
-// 	return (1);
-// }
+void execute(const char *argv, char **envp)
+{
+	char **flags;
+	(void)argv;
+	(void)envp;
+	flags = ft_split(argv, ' ');
+	if (flags == NULL) // Alors utiliser argv
+	// execve();
+}
 
-int	main(int argc, char **argv, char **envp)
+void	use_perror(char *str)
+{
+	perror(str);
+	exit(EXIT_FAILURE);
+}
+
+void	first_cmd(int *pipefd, const char **argv, char **envp)
+{
+	int first_pid;
+
+	first_pid = open(argv[1], O_RDONLY);
+	if (first_pid < 0)
+		args_error();
+	close(pipefd[0]);
+	dup2(first_pid, STDIN_FILENO);
+	dup2(pipefd[1], STDOUT_FILENO);
+	execute(argv[1], envp);
+}
+
+void	second_cmd(int *pipefd,const char **argv, char **envp)
+{
+	int second_pid;
+
+	second_pid = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (second_pid < 0)
+		args_error();
+	close(pipefd[0]);
+	dup2(second_pid, STDOUT_FILENO);
+	dup2(pipefd[0], STDIN_FILENO);
+	execute(argv[3], envp);
+}
+
+int	main(int argc,const char **argv, char **envp)
 {
 	int pipefd[2];
-	int in_pid;
-	int first_pid;
-	int second_pid;
-	int *status;
-	int i;
+	int ret;
+	int status;
+	int child;
+	int child2;
 
-	i = 1;
-	(void)envp;
 	if (argc != 5)
 		args_error();
-	if (pipe(pipefd) == -1)
-	{
-		perror("pipe");
-		exit(EXIT_FAILURE);
-    }
+	ret = pipe(pipefd);
+	if (ret == -1)
+		use_perror("pipe");
 	child = fork();
     if (child < 0)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-    }
+		use_perror("fork");
 	if (child == 0)
-	{
-		first_pid = open(argv[1], O_RDONLY);
-		if (first_pid < 0)
-			args_error();
-	}
+		first_cmd(pipefd, argv, envp);
 	child2 = fork();
 	if (child2 < 0)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
+		use_perror("fork");
 	if (child2 == 0)
-	{
-		second_pid = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
-		if (second_pid < 0)
-			args_error();
-	}
+		second_cmd(pipefd, argv, envp);
 	waitpid(child, &status, 0);
-	close(pipefd[0]);
-	waitpid(child2, &status, 0);
 	close(pipefd[1]);
+	waitpid(child2, &status, 0);
+	close(pipefd[0]);
 	return (0);
 }
